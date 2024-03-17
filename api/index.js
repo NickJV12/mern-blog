dd8b52269e910600c23bc75ef8f8af85b48c7b3e
NickJV12 <shambhavim1202@gmail.com>
2024-03-18 01:01:58 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,3 +18,6 @@ app.listen(8000, ()=> {
 
     console.log('Server is running on port 8000');
 })
+
+//API ROUTE
+app.use('/api/user', userRoutes);
