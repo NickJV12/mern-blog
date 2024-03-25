bab9310abe5c17c3de17ada4ff9ada2ce1bf72bb
Priyal <priyalsingh312@gmail.com>
2024-03-25 13:19:57 +0530

    add fuctionality to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,6 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
@@ -44,12 +43,12 @@ export default function SignUp() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Case
+              Sahand's
             </span>
             Blog
           </Link>
           <p className='text-sm mt-5'>
-            This is a blog page. You can sign up with your email and password
+            This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
         </div>
@@ -98,7 +97,6 @@ export default function SignUp() {
                 'Sign Up'
               )}
             </Button>
-            
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
