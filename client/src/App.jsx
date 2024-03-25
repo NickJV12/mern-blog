7aafe5b9b624f9fec955718fcaeb9d6664fc5a6a
NickJV12 <shambhavim1202@gmail.com>
2024-03-25 16:36:43 +0530

    add functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,6 @@ export default function App() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/projects" element={<Projects />} />
     </Routes>
-    <Footer/>
     </BrowserRouter>
   )
 }
