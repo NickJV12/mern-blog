0b4cb597c84b21d57c6117fd9cda1fa06b62513c
NickJV12 <shambhavim1202@gmail.com>
2024-06-12 19:06:16 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -13,7 +13,8 @@ import {
   updateFailure,
   deleteUserStart,
   deleteUserFailure,
-  deleteUserSuccess } from '../redux/user/userSlice';
+  deleteUserSuccess,
+  signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { Modal } from 'flowbite-react/lib/cjs/components/Modal';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
@@ -139,6 +140,22 @@ export default function DashProfile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if(!res.ok) {
+        console.log(data.message);
+      } else {
+         dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return(
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -193,14 +210,14 @@ export default function DashProfile() {
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-        <span className='cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
       </div>
       {updateUserSuccess && (
         <Alert color='success' className='mt-5'>
           {updateUserSuccess}
         </Alert>
       )}
-      {updateUserSuccess && (
+      {updateUserError && (
         <Alert color='failure' className='mt-5'>
           {updateUserError}
         </Alert>
