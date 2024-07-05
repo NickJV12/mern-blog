cba8415c49c1c51d8e0b66da51db24a3d39cef56
NickJV12 <shambhavim1202@gmail.com>
2024-07-05 11:15:16 +0530

    add comments to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiUser,HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
+import { HiArrowSmRight, HiUser,HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -59,6 +59,7 @@ export default function DashSidebar() {
                 </Link>
                 )}
                 {currentUser.isAdmin && (
+                  <>
                   <Link to='/dashboard?tab=users'>
                     <Sidebar.Item
                     active={tab === 'users'}
@@ -68,6 +69,16 @@ export default function DashSidebar() {
                       Users
                     </Sidebar.Item>
                   </Link>
+                  <Link to='/dashboard?tab=comments'>
+                  <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon = {HiAnnotation}
+                  as='div'
+                  >
+                    Comments
+                  </Sidebar.Item>
+                </Link>
+                </>
                 )}
                 <Sidebar.Item active icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                    Sign Out
